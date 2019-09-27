638c0838243be5062def5c5a7436feeffd15a136
Piti Irawan <piti.irawan@gmail.com>
2019-09-26 18:18:46 -0700

    Fixed scroll view zoom.

--- PersistentGallery/GalleryScrollViewController.swift
+++ PersistentGallery/GalleryScrollViewController.swift
@@ -20,9 +20,7 @@ class GalleryScrollViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
-        scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 1.0
-        scrollView.zoomScale = 1.0
+        scrollView.maximumZoomScale = 1.2
         imageView.image = image
         imageView.frame = CGRect(origin: CGPoint.zero, size: image.size)
         scrollView.contentSize = image.size
@@ -30,20 +28,20 @@ class GalleryScrollViewController: UIViewController, UIScrollViewDelegate {
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        let widthScale = view.bounds.size.width / imageView.bounds.width
-        let heightScale = view.bounds.size.height / imageView.bounds.height
+        let widthScale = view.bounds.width / imageView.bounds.width
+        let heightScale = view.bounds.height / imageView.bounds.height
         let minScale = min(widthScale, heightScale)
-        scrollView.minimumZoomScale = minScale
-        scrollView.zoomScale = minScale
+        scrollView.minimumZoomScale = min(0.8, minScale)
+        scrollView.zoomScale = min(0.99, minScale)
     }
     
     // MARK: - UIScrollViewDelegate
 
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        let yOffset = max(0, (view.bounds.size.height - imageView.frame.height) / 2)
+        let yOffset = max(0, (view.bounds.height - imageView.frame.height) / 2)
         imageViewTopConstraint.constant = yOffset
         imageViewBottomConstraint.constant = yOffset
-        let xOffset = max(0, (view.bounds.size.width - imageView.frame.width) / 2)
+        let xOffset = max(0, (view.bounds.width - imageView.frame.width) / 2)
         imageViewLeadingConstraint.constant = xOffset
         imageViewTrailingConstraint.constant = xOffset
         view.layoutIfNeeded()
